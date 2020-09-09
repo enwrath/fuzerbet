0b1613f126203bb6a079a8cae5a3d6cdec6642e8
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-09-09 20:47:49 +0300

    typo

--- betsite/betsite/urls.py
+++ betsite/betsite/urls.py
@@ -5,5 +5,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('', include('betapp.urls')),
     path('admin/', admin.site.urls),
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
