1527a322a69edf58ba91191229db0470c7a7b0b5
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-09-09 20:27:30 +0300

    favico

--- betsite/betsite/urls.py
+++ betsite/betsite/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('betapp.urls')),
     path('admin/', admin.site.urls),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico')),
 ]
