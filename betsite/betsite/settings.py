f3d32bd97900b9330ac085f050fdbe20db57b8a8
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-09-09 20:32:49 +0300

    db dix

--- betsite/betsite/settings.py
+++ betsite/betsite/settings.py
@@ -98,6 +98,7 @@ WSGI_APPLICATION = 'betsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {}
 DATABASES = dj_database_url.config(conn_max_age=600, ssl_require=True)
 #DATABASES = {
 #    'default': {
