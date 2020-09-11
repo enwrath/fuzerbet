cbc30287982466b3d5ce6f12edb099ca1e527f4a
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-09-11 21:47:51 +0300

    uncommented comment lol

--- betsite/betsite/settings.py
+++ betsite/betsite/settings.py
@@ -24,7 +24,7 @@ https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-Extra places for collectstatic to find static files.
+#Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
