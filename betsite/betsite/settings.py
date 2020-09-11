fd852e3203a9ac71555aa42d5f55b99687dff19e
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-09-11 21:48:38 +0300

    reee

--- betsite/betsite/settings.py
+++ betsite/betsite/settings.py
@@ -19,8 +19,8 @@ import dj_database_url
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-Static files (CSS, JavaScript, Images)
-https://docs.djangoproject.com/en/1.9/howto/static-files/
+#Static files (CSS, JavaScript, Images)
+#https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
